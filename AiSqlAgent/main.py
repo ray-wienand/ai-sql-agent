923a688b9259f28ed89c7212a1516bd3a165e25b
ray-wienand <raymond@wienand.co.za>
2024-06-25 19:42:50 +0200

    Sarted Streamlit frontend

--- AiSqlAgent/main.py
+++ AiSqlAgent/main.py
@@ -2,6 +2,46 @@ from sql_chain import execute_queries_with_retries
 
 import os
 import streamlit as st
+import pandas as pd
+
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
+from groq import Groq
+from langchain.chains import ConversationChain, LLMChain
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain_core.messages import SystemMessage
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+
+from langchain_experimental.agents import create_pandas_dataframe_agent
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm_model = "llama3-8b-8192"
+llm_temperature = 0
+
+api_key = os.environ.get("GROQ_API_KEY")
+
+llm = ChatGroq(model=llm_model, temperature=llm_temperature, api_key=api_key)
+
+# Used this for a single non-memory chain in the sidebar
+client = Groq(api_key=api_key)
+
+system = "{system_text}"
+human = "{human_text}"
+prompt = ChatPromptTemplate.from_messages([("human", human), ("system", system)])
+chain = prompt | llm
+
+# Remembers the last 10 messages
+conversational_memory_length = 100
+memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
 
 
 # Main
@@ -20,6 +60,51 @@ with st.sidebar:
     st.divider()
 
     st.caption("<p style = 'text-align: center'> Developed by Ray Wienand </p>", unsafe_allow_html=True)
+
+
+# Initialise a session state variable
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = {1: False}
+
+# Function to update the value in session state
+def clicked(button):
+    st.session_state.clicked[button] = True
+
+# Upload CSV file for analysis
+st.button("Let's get started", on_click=clicked, args=[1])
+if st.session_state.clicked[1]:    
+    st.header('Exploratory Data Analysis')
+    st.subheader('Solution')
+
+    user_csv = st.file_uploader("Upload your file here", type="csv")  
+
+    # if user_csv is not None:
+    #     # Ensure pointer is at the start of the file
+    #     user_csv.seek(0)
+    #     df = pd.read_csv(user_csv, low_memory=False)
+        
+    #     # Create the pandas agent after df is defined
+    #     pandas_agent = create_pandas_dataframe_agent(llm, df, allow_dangerous_code=True, verbose=True)
+    #     question = 'What is the meaning of the columns'
+    #     columns_meaning = pandas_agent.run(question)
+    #     st.write(columns_meaning)
+
+
+# NOTE! Initiated a single call to groq as to NOT add this to the memory chain
+with st.sidebar:
+    with st.expander('What are the steps of Data Analysis?'):        
+        # LLM provided steps        
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "What are the steps of Data Analysis?",
+                }
+            ],
+            model="llama3-8b-8192",
+            temperature=0
+        )
+        st.write(chat_completion.choices[0].message.content)
 
 
 my_query = """
